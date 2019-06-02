f6f2b97abfcfd31d25490166238dd0267a716703
wjh624 <1412106667@qq.com>
2019-06-03 00:00:11 +0800

    1. 创建菜单栏 2. 初始设计profile页面

---
+++ earnMoney/pages/profile/userInfo/userInfo.js
@@ -0,0 +1,71 @@
+// pages/profile/userInfo/userInfo.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    userInfo: {
+      status: 0,
+      name: 'WJH',
+      email: '7777777@qq.com',
+      school: 'SYSU'
+    } // 需要后端
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
